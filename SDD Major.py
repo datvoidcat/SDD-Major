06602008ed68c5c42c4997d3028bb7a4f84d5d1d
datvoidcat <alexandersoo111@gmail.com>
2022-08-20 08:59:28 +1000

    Updated some variables
    I changed the names of some variables to better suit their purpose

--- SDD Major.py
+++ SDD Major.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 import numpy as np
+from matplotlib.widgets import Button
 
 GREY = (0.78, 0.78, 0.78)  # Healthy
 RED = (0.96, 0.15, 0.15)   # Infected
@@ -10,8 +11,8 @@ BLACK = (0, 0, 0)          # Dead
 Covid_Settings = {
     "r0": 2.28,
     "incubation": 5,
-    "percent_mild": 0.8,
-    "mild_recovery": (7, 14),
+    "percent_weak": 0.8,
+    "weak_recovery": (7, 14),
     "percent_strong": 0.2,
     "strong_recovery": (21, 42),
     "strong_death": (14, 56),
@@ -42,31 +43,31 @@ class Virus():
 
         # create variables
         self.day = 0
-        self.total_num_infected = 0
+        self.total_infected = 0
         self.num_currently_infected = 0
         self.num_recovered = 0
         self.num_deaths = 0
         self.r0 = params["r0"]
-        self.percent_mild = params["percent_mild"]
+        self.percent_weak = params["percent_weak"]
         self.percent_strong = params["percent_strong"]
         self.fatality_rate = params["fatality_rate"]
         self.serial_interval = params["serial_interval"]
 
-        self.mild_fast = params["incubation"] + params["mild_recovery"][0]
-        self.mild_slow = params["incubation"] + params["mild_recovery"][1]
+        self.weak_fast = params["incubation"] + params["weak_recovery"][0]
+        self.weak_slow = params["incubation"] + params["weak_recovery"][1]
         self.strong_fast = params["incubation"] + params["strong_recovery"][0]
         self.strong_slow = params["incubation"] + params["strong_recovery"][1]
         self.death_fast = params["incubation"] + params["strong_death"][0]
         self.death_slow = params["incubation"] + params["strong_death"][1]
 
-        self.mild = {i: {"thetas": [], "rs": []} for i in range(self.mild_fast, 365)}
+        self.weak = {i: {"thetas": [], "rs": []} for i in range(self.weak_fast, 365)}
         self.strong = {
             "recovery": {i: {"thetas": [], "rs": []} for i in range(self.strong_fast, 365)},
             "death": {i: {"thetas": [], "rs": []} for i in range(self.death_fast, 365)}
         }
 
-        self.exposed_before = 0
-        self.exposed_after = 1
+        self.affected_before = 0
+        self.affected_after = 1
 
         self.starting_population()
 
@@ -77,30 +78,30 @@ class Virus():
     def starting_population(self):
         population = 4200
         self.num_currently_infected = 1
-        self.total_num_infected = 1
+        self.total_infected = 1
         indices = np.arange(0, population) + 0.5
         self.thetas = np.pi * (1 + 5**0.5) * indices
         self.rs = np.sqrt(indices / population)
         self.plot = self.axes.scatter(self.thetas, self.rs, s=5, color=GREY)
-        # assign patient zero
+        # assign the middle person to be patient zero
         self.axes.scatter(self.thetas[0], self.rs[0], s=5, color=RED)
-        self.mild[self.mild_fast]["thetas"].append(self.thetas[0])
-        self.mild[self.mild_fast]["rs"].append(self.rs[0])
+        self.weak[self.weak_fast]["thetas"].append(self.thetas[0])
+        self.weak[self.weak_fast]["rs"].append(self.rs[0])
 
 
     def spread_virus(self, i):
-        self.exposed_before = self.exposed_after
-        if self.day % self.serial_interval == 0 and self.exposed_before < 4200:
-            self.num_new_infected = round(self.r0 * self.total_num_infected)
-            self.exposed_after += round(self.num_new_infected * 1.1)
-            if self.exposed_after > 4200:
-                self.num_new_infected = round((4200 - self.exposed_before) * 0.9)
-                self.exposed_after = 4200
+        self.affected_before = self.affected_after
+        if self.day % self.serial_interval == 0 and self.affected_before < 4200:
+            self.num_new_infected = round(self.r0 * self.total_infected)
+            self.affected_after += round(self.num_new_infected * 1.1)
+            if self.affected_after > 4200:
+                self.num_new_infected = round((4200 - self.affected_before) * 0.9)
+                self.affected_after = 4200
             self.num_currently_infected += self.num_new_infected
-            self.total_num_infected += self.num_new_infected
+            self.total_infected += self.num_new_infected
             self.new_infected_indices = list(
                 np.random.choice(
-                    range(self.exposed_before, self.exposed_after),
+                    range(self.affected_before, self.affected_after),
                     self.num_new_infected,
                     replace=False))
             thetas = [self.thetas[i] for i in self.new_infected_indices]
@@ -152,7 +153,7 @@ class Virus():
 
 
     """
-        We randomly assign a subset of the newly infected to have mild symptoms, and the rest to have
+        We randomly assign a subset of the newly infected to have weak symptoms, and the rest to have
         strong symptoms. 
         
         We then randomly assign a recovery or death day to each of the newly infected. 
@@ -161,23 +162,23 @@ class Virus():
         
         The range of days is determined by the fast and slow recovery/death days. 
         
-        The fast and slow recovery/death days are determined by the mild_fast, mild_slow, strong_fast,
+        The fast and slow recovery/death days are determined by the weak_fast, weak_slow, strong_fast,
         strong_slow, death_fast, and death_slow parameters. 
         
         The fast and slow recovery/death days are randomly chosen from a range of days. 
         
         The range of days is determined by the fast and slow recovery/death days. 
         
-        The fast and slow recovery/death days are determined by the mild_fast, mild_slow, strong_fast,
+        The fast and slow recovery/death days are determined by the weak_fast, weak_slow, strong_fast,
         strong_slow, death_fast, and death_slow parameters
         """
     def assign_symptoms(self):
-        num_mild = round(self.percent_mild * self.num_new_infected)
+        num_weak = round(self.percent_weak * self.num_new_infected)
         num_strong = round(self.percent_strong * self.num_new_infected)
-        # choose random subset of newly infected to have mild symptoms
-        self.mild_indices = np.random.choice(self.new_infected_indices, num_mild, replace=False)
+        # choose random subset of newly infected to have weak symptoms
+        self.weak_indices = np.random.choice(self.new_infected_indices, num_weak, replace=False)
         # assign the rest strong symptoms, either resulting in recovery or death
-        remaining_indices = [i for i in self.new_infected_indices if i not in self.mild_indices]
+        remaining_indices = [i for i in self.new_infected_indices if i not in self.weak_indices]
         percent_strong_recovery = 1 - (self.fatality_rate / self.percent_strong)
         num_strong_recovery = round(percent_strong_recovery * num_strong)
         self.strong_indices = []
@@ -187,14 +188,14 @@ class Virus():
             self.death_indices = [i for i in remaining_indices if i not in self.strong_indices]
 
         # assign recovery/death day
-        low = self.day + self.mild_fast
-        high = self.day + self.mild_slow
-        for mild in self.mild_indices:
+        low = self.day + self.weak_fast
+        high = self.day + self.weak_slow
+        for weak in self.weak_indices:
             recovery_day = np.random.randint(low, high)
-            mild_theta = self.thetas[mild]
-            mild_r = self.rs[mild]
-            self.mild[recovery_day]["thetas"].append(mild_theta)
-            self.mild[recovery_day]["rs"].append(mild_r)
+            weak_theta = self.thetas[weak]
+            weak_r = self.rs[weak]
+            self.weak[recovery_day]["thetas"].append(weak_theta)
+            self.weak[recovery_day]["rs"].append(weak_r)
         low = self.day + self.strong_fast
         high = self.day + self.strong_slow
         for recovery in self.strong_indices:
@@ -213,18 +214,18 @@ class Virus():
             self.strong["death"][death_day]["rs"].append(death_r)
 
     """
-        If the day is greater than the day that the mild cases are supposed to recover, then plot the
-        mild cases as green(recovered). If the day is greater than the day that the strong cases are supposed to
+        If the day is greater than the day that the weak cases are supposed to recover, then plot the
+        weak cases as green(recovered). If the day is greater than the day that the strong cases are supposed to
         recover, then plot the strong cases as green(recovered). If the day is greater than the day that the strong
         cases are supposed to die, then plot the strong cases as black(dead)
         """
     def update_status(self):
-        if self.day >= self.mild_fast:
-            mild_thetas = self.mild[self.day]["thetas"]
-            mild_rs = self.mild[self.day]["rs"]
-            self.axes.scatter(mild_thetas, mild_rs, s=5, color=GREEN)
-            self.num_recovered += len(mild_thetas)
-            self.num_currently_infected -= len(mild_thetas)
+        if self.day >= self.weak_fast:
+            weak_thetas = self.weak[self.day]["thetas"]
+            weak_rs = self.weak[self.day]["rs"]
+            self.axes.scatter(weak_thetas, weak_rs, s=5, color=GREEN)
+            self.num_recovered += len(weak_thetas)
+            self.num_currently_infected -= len(weak_thetas)
         if self.day >= self.strong_fast:
             rec_thetas = self.strong["recovery"][self.day]["thetas"]
             rec_rs = self.strong["recovery"][self.day]["rs"]
@@ -247,11 +248,11 @@ class Virus():
 
 
     def gen(self):
-        while self.num_deaths + self.num_recovered < self.total_num_infected:
+        while self.num_deaths + self.num_recovered < self.total_infected:
             yield
 
 
-    def animate(self):
+    def animated(self):
         self.anim = ani.FuncAnimation(
             self.fig,
             self.spread_virus,
@@ -261,7 +262,7 @@ class Virus():
 
 def main():
     coronavirus = Virus(Covid_Settings)
-    coronavirus.animate()
+    coronavirus.animated()
     plt.show()
 
 
